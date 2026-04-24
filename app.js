1e7ab41986f00984b5b47c45bcb43b04b5b6dfd8
Rohit 1818 <rtingane2611@gmail.com>
2026-04-25 02:17:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.end("Hello from Developer 🚀");
+  res.end("Hello from DevOps Auto Deploy 🚀");
 });
 
 server.listen(3000, () => {
